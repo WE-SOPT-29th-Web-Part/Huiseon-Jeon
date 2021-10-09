ee2be432b5c78db655e318a05a9d8543deb461e7
Huiseon Jeon <huiseon37@gmail.com>
2021-10-09 18:57:29 +0900

    [FEAT] to-do-list JS 구현

---
+++ to-do-list/main.js
@@ -0,0 +1,65 @@
+"use strict";
+//엄격모드
+
+const inputs = document.querySelectorAll(".todos__input");
+const addBtns = document.querySelectorAll(".todos__add");
+const allItems = document.querySelectorAll(".todos__items");
+
+//보통 화살표 함수로 사용함
+addBtns.forEach((btn,index) => {
+    btn.addEventListener("click",() => {
+        onAdd(index);
+    });
+});
+
+inputs.forEach((input,index) => {
+    input.addEventListener("keyup", (e) => {
+        if(e.key==="Enter"){
+            onAdd(index);
+        }  
+    });
+});
+
+const onAdd = (index) =>{
+    if(!inputs[index].value) return;
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+        //const checkBox = document.createElement("input");
+
+    li.setAttribute("class", "todos__item");
+    span.setAttribute("class", "todos__name");
+    deleteBtn.setAttribute("class", "todos__delete");
+        //checkBox.setAttribute("type", "checkbox");
+
+    deleteBtn.innerText = "X";
+        //span.appendChild(checkBox);
+    span.innerText = inputs[index].value;
+        
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    allItems[index].appendChild(li);
+
+    inputs[index].value = "";
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+}
+
+const nav = document.querySelector(".options");
+const todos = document.querySelectorAll(".todos > section");
+
+nav.addEventListener("click",(event) => {
+    if(event.target.className.includes("options__today")){
+        todos[0].classList.add("open");
+        todos[1].classList.remove("open");
+    }else if(event.target.className.includes("options__tomorrow")){
+        todos[1].classList.add("open");
+        todos[0].classList.remove("open");
+    }else if(event.target.className.includes("options__both")){
+        todos[1].classList.add("open");
+        todos[0].classList.add("open");
+    }
+});
+
