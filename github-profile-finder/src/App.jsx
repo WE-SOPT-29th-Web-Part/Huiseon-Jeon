d303ab9f57e9c275a0b0226b9212973ae76a6e6a
Huiseon Jeon <huiseon37@gmail.com>
2021-10-30 05:57:13 +0900

    [FEAT] api 로 github 정보 가져오는 기능 구현

---
+++ github-profile-finder/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import Header from './components/Header';
+import Result from './components/Result';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  const [userInfo, setUserInfo] = useState("");
+
+  useEffect(() => {
+    console.log(`userInfo`,userInfo);
+  }, [userInfo]);
+
+  return (
+    <Root>
+      <Header/>
+      <SearchBar setUserInfo={setUserInfo}/>
+      <Result userInfo={userInfo}/>
+    </Root>
+  );
+}
+
+export default App;
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
---
+++ github-profile-finder/src/components/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Result = ({userInfo}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Result;
